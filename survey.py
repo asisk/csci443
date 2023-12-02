bbf1421601984f9c8b894c4fc177ed40527dc507
asisk <anniesisk@gmail.com>
2023-12-02 14:16:43 -0700

    working solution producing stacked bar chart

--- survey.py
+++ survey.py
@@ -5,32 +5,46 @@ import seaborn as sns
 def main():
     # Import CSV data using pandas
     df = pd.read_csv('surveyData.csv')
-    # Get the number of columns
-    # total_columns = df.shape[1]
-    # print (total_columns)
+    
+    # Specify the Likert scale columns (Q1 to Q10)
+    likert_columns = [f'Q{i}' for i in range(1, 11)]
 
-    likert_scale_values = [1,2,3,4,5]
-    # Print the values of each identified Likert scale column
-    # Identify columns with Likert scale values
+   # Output mean and standard deviation for each column
+    for column in likert_columns:
+        try:
+            # Exclude non-numeric values before calculations
+            numeric_values = pd.to_numeric(df[column], errors='coerce')
+            numeric_values = numeric_values[numeric_values.notna()]  # Exclude NaN values
+            
+            # Calculate mean and standard deviation
+            column_mean = numeric_values.mean()
+            column_std = numeric_values.std()
+
+            print(f"Column: {column}, Mean: {column_mean}, Std Dev: {column_std}")
+        except ValueError as e:
+            print(f"Error processing column {column}: {e}")
+
+    likert_scale_values = [1, 2, 3, 4, 5]
     likert_columns = []
-    for col in df.columns[18:27]:
+
+    # Identify Likert scale columns
+    for col in df.columns[17:27]:
         try:
             numeric_values = pd.to_numeric(df[col], errors='coerce')
-            if numeric_values[numeric_values.notna()].isin([1, 2, 3, 4, 5]).all():
-                print(f"Likert scale column found: {col}")
+            if numeric_values[numeric_values.notna()].isin(likert_scale_values).all():
+                # print(f"Likert scale column found: {col}")
                 likert_columns.append(col)
         except ValueError as e:
             print(f"Error processing column {col}: {e}")
 
-    # Check if there are Likert scale columns
     if not likert_columns:
         print("No Likert scale columns found.")
         return
-
-    # Step 3: Create a DataFrame to store the counts
+    
+    # Create a DataFrame to store the counts
     likert_counts = pd.DataFrame(index=likert_scale_values, columns=likert_columns)
 
-    # Step 4: Populate the DataFrame with counts
+    # Populate the DataFrame with counts
     for column in likert_columns:
         try:
             # Exclude non-numeric values before converting to integers
@@ -41,15 +55,18 @@ def main():
         except ValueError as e:
             print(f"Error processing column {column}: {e}")
 
-    # Step 5: Plot the stacked bar chart
+    # Plot the stacked bar chart
     plt.figure(figsize=(14, 8))
     likert_counts.T.plot(kind='bar', stacked=True, colormap='viridis')
-    
-    plt.title('Likert Scale Distribution for Each Question')
+
+    plt.title('SUS Likert Distribution')
     plt.xlabel('Questions')
     plt.ylabel('Count')
+
+    # Handle NaN values in the legend
+    plt.legend(title='Likert Scale', loc='upper left', bbox_to_anchor=(1, 1))
+
     plt.xticks(rotation=45, ha='right')
-    plt.legend(title='Likert Scale')
     plt.show()
 
 if __name__ == "__main__":
